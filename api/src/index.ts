3aba98105bce9e20ce3c1624ebe00c0829038626
piresrisrael <piresr.israel@gmail.com>
2022-02-19 13:35:53 -0300

    feat(error): add an error handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 import express from 'express';
 import { createConnection } from 'typeorm';
 import log from 'utils';
 import router from 'routes';
+import { errorMiddleware } from 'utils/ErrorHandler';
 
 createConnection();
 
@@ -11,6 +13,8 @@ const server = express();
 server.use(express.json({ limit: '2mb' }));
 
 server.use('/api/v1', router);
+
+server.use(errorMiddleware);
 
 server.listen(3080, () => {
   // eslint-disable-next-line no-console
---
+++ api/src/utils/ErrorHandler.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import log from 'utils';
+
+interface ErrorParams {
+  id: string;
+  message: string;
+  statusCode: number;
+}
+export default class ErrorHandler {
+  public readonly id: string;
+
+  public readonly message: string;
+
+  public readonly statusCode: number;
+
+  constructor({ id, message, statusCode = 400 }: ErrorParams) {
+    this.id = id;
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export function errorMiddleware(
+  error: Error,
+  request: Request,
+  response: Response,
+  _: NextFunction,
+): Response {
+  if (error instanceof ErrorHandler) {
+    log.error(error.message);
+    return response.status(error.statusCode).json({
+      status: error.statusCode,
+      id: error.id,
+      message: error.message,
+    });
+  }
+
+  log.error(error);
+  // eslint-disable-next-line no-console
+  console.log(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+}
